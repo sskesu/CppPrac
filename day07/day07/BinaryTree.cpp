ba95423cde2b35aa45e4e6207ec3b8de978776cf
sskesu <136444697+sskesu@users.noreply.github.com>
2023-08-24 18:36:16 +0900

    Add files via upload

--- day07/day07/BinaryTree.cpp
+++ day07/day07/BinaryTree.cpp
@@ -2,41 +2,53 @@
 
 void BinaryTree::AddNode(int data)
 {
+	Node* newNode = CreateNode(data);
 	if (rootNode == nullptr) {
-		rootNode->data = data;
+		rootNode = newNode;
 	}
 	else {
-		Node* temp = new Node;
-		temp = rootNode;
-		while (true) {
-			if (data < temp->data && temp->left == nullptr) {
-				Node* newNode = CreateNode(data);
-				temp->left = newNode;
-				break;
-			}
-			else if (data >= temp->data && temp->right == nullptr) {
-				Node* newNode = CreateNode(data);
-				temp->right = newNode;
-				break;
-			}
-			else if (data < temp->data) {
-				temp = temp->left;
-			}
-			else {
-				temp = temp->right;
-			}
-		}
-		
+		InsertNode(rootNode, newNode);
 	}
 }
 
 void BinaryTree::RemoveNode(int data)
 {
-
+	if (rootNode->data == data) {
+		rootNode->data = -99;
+	}
+	else {
+		Node* newNode = new Node;
+		Node* temp = new Node;
+		newNode = rootNode;
+		
+		while (true) {
+			if (newNode->data > data) {
+				temp = newNode->left;
+				if (temp->data == data) {
+					temp = DeleteNode(temp, data);
+					delete newNode->left;
+					newNode->left = temp;
+					break;
+				}
+				newNode = temp;
+			}
+			else {
+				temp = newNode->right;
+				if (temp->data == data) {
+					temp = DeleteNode(temp, data);
+					delete newNode->right;
+					newNode->right = temp;
+					break;
+				}
+				newNode = temp;
+			}
+		}
+	}
 }
 
 void BinaryTree::PrintAll()
 {
+	InOrder(rootNode);
 }
 
 Node* BinaryTree::CreateNode(int data)
@@ -46,20 +58,47 @@ Node* BinaryTree::CreateNode(int data)
 		nullptr,
 		data,
 	};
-	return nullptr;
+	return newNode;
 }
 
 void BinaryTree::InsertNode(Node* tree, Node* newNode)
 {
+	if (tree->data > newNode->data) {
+		if (tree->left == nullptr) {
+			tree->left = newNode;
+		}
+		else {
+			InsertNode(tree->left, newNode);
+		}
+	}
+	else {
+		if (tree->right == nullptr) {
+			tree->right = newNode;
+		}
+		else {
+			InsertNode(tree->right, newNode);
+		}
+	}
 
 }
 
-Node* BinaryTree::DeleteNode(Node* tree, int data)
+Node* BinaryTree::DeleteNode(Node* newNode, int data)
 {
-	if (tree->data == data) {
-		delete tree;
+	Node* temp = new Node;
+
+	if (newNode->left == nullptr && newNode->right == nullptr) {
+		temp = nullptr;
 	}
-	return nullptr;
+	else if (newNode->left != nullptr && newNode->right == nullptr) {
+		temp = newNode->left;
+	}
+	else if (newNode->left == nullptr && newNode->right != nullptr) {
+		temp = newNode->right;
+	}
+	else {
+
+	}
+	return temp;
 }
 
 Node* BinaryTree::FindMin(Node* root)
@@ -69,6 +108,13 @@ Node* BinaryTree::FindMin(Node* root)
 
 void BinaryTree::InOrder(Node* root)
 {
+	if (root == nullptr) {
+		return;
+	}
+
+	InOrder(root->left);
+	cout << root->data << endl;
+	InOrder(root->right);
 }
 
 BinaryTree::BinaryTree()
--- day07/day07/Cmain.cpp
+++ day07/day07/Cmain.cpp
@@ -1,6 +1,23 @@
 #include "BinaryTree.h"
 
 int main() {
+	BinaryTree BT;
+
+	BT.AddNode(20);
+	BT.AddNode(10);
+	BT.AddNode(15);
+	BT.AddNode(25);
+	BT.AddNode(17);
+	BT.AddNode(30);
+	BT.AddNode(32);
+
+	BT.PrintAll();
+	cout << "天天天天天天" << endl;
+
+	//
+	BT.RemoveNode(30);
+	BT.RemoveNode(32);
+	BT.PrintAll();
 
 	return 0;
 }
